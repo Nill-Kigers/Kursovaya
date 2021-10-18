feccbe79ac1cfd0fc1324e7565ee0d52bc682ce8
Nill-Kigers <90038624+Nill-Kigers@users.noreply.github.com>
2021-10-18 03:00:02 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyUtil
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void tsmiExit_Click(object sender, EventArgs e)
+        {
+            this.Close(); //присвоение функции выход
+        }
+
+        private void tsmiAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Программа содержит ряд небольших программ," +
+                "\n которые могут пригодиться в жизни.", "О программе");
+            //после запятой - название окна; \n - перенос на другую строчку
+        }
+    }
+}
